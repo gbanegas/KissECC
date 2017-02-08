4420cfc0f53bf74d9ae9dfc9432630da9835d9b6
Gustavo Banegas <gustavo@RISCURE.COM>
2017-02-08 17:38:52 +0100

    add data to the ww-attack

--- attack_ecc/algo.py
+++ attack_ecc/algo.py
@@ -3,10 +3,19 @@ n = 250
 
 window_size = 10
 
-def generate_d_candidates(difference):
+def generate_d_candidates(difference, bit_size):
+    l =[]
+    general_zeros = [0]*(bit_size-difference)
     for i in product([0,1], repeat=difference):
-        print i
-    new_l = l+bin_list
+        l.append(list(i))
+
+    all_combinations = []
+    for w_bits_list in l:
+        new_l = w_bits_list+general_zeros
+        all_combinations.append(new_l)
+
+    return all_combinations
+
     #print new_l
 
 
@@ -18,10 +27,15 @@ def wide_widow_attack():
     w = window_size
     d_l = 0
 
-    while(w > n):
-        d_candidates = generate_d_candidates(w-w_l)
+    while(w < n):
+        print "Interaction"
+        bits_size = 2**2
+
+        d_candidates = generate_d_candidates(w-w_l, bits_size)
         w_l = w
         w = w + window_size
+        print d_candidates
+
 
 
 
