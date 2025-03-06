9f8f46c63b1ce915791af748468f5a84217b3f5d
Gustavo Banegas <gustavo@cryptme.in>
2025-03-06 13:58:51 +0100

    updated to python 3

--- signatureerror.py
+++
@@ -1,6 +0,0 @@
-
-class SignatureError(Exception):
-    def __init__(self, vef, sig):
-        super.__init__(self,"Invalid signature: {0} <> {1} ".format(vef, sig))
-        self.vef = vef
-        self.sig = sig
